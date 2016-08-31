be5690f0b2b2412d30079abf8c6267bfef114c19
chorlamata <maria.lupanova85@gmail.com>
2016-08-31 20:41:27 +0300

    Add files via upload

---
+++ Aries-Blog/scripts/app.js
@@ -0,0 +1,86 @@
+(function () {
+
+    // Create your own kinvey application
+
+    let baseUrl = "https://baas.kinvey.com";
+    let appKey = "kid_rkXXFjTc"; // Place your appKey from Kinvey here...
+    let appSecret = "bc4174cf1e914b9ca742a1c67065ae68"; // Place your appSecret from Kinvey here...
+    let _guestCredentials = "029cf568-e0e7-4467-be94-3271c35ef4b4.zYYHw8WPxSraBqK6YzkwCb7Ig2hCPZz8ZRtKUBvNJUM="; // Create a guest user using PostMan/RESTClient/Fiddler and place his authtoken here...
+
+    //Create AuthorizationService and Requester
+
+    let selector = ".wrapper";
+    let mainContentSelector = ".main-content";
+
+    let authService = new AuthorizationService(baseUrl, appKey, appSecret, _guestCredentials);
+    authService.initAuthorizationType("Kinvey");
+
+    let requester = new Requester(authService);
+
+    // Create HomeView, HomeController, UserView, UserController, PostView and PostController
+    let homeView = new HomeView(selector,mainContentSelector);
+    let homeController = new HomeController(homeView, requester, baseUrl, appKey);
+    let userView = new UserView(selector,mainContentSelector);
+    let userController = new UserController(userView, requester, baseUrl, appKey);
+    let postView = new PostView(selector,mainContentSelector);
+    let postController = new PostController(postView, requester, baseUrl, appKey);
+
+
+    initEventServices();
+
+    onRoute("#/", function () {
+        // Check if user is logged in and if its not show the guest page, otherwise show the user page...
+        if (!authService.isLoggedIn()){
+            homeController.showGuestPage();
+        }
+        else{
+            homeController.showUserPage();
+        }
+    });
+
+    onRoute("#/post-:id", function () {
+        // Create a redirect to one of the recent posts...
+        let top = $("#post-" + this.params['id']).position().top;
+        $(window).scrollTop(top);
+    });
+
+    onRoute("#/login", function () {
+        // Show the login page...
+        userController.showLoginPage(authService.isLoggedIn());
+    });
+
+    onRoute("#/register", function () {
+        // Show the register page...
+        userController.showRegisterPage(authService.isLoggedIn());
+    });
+
+    onRoute("#/logout", function () {
+        // Logout the current user...
+        userController.logout();
+    });
+
+    onRoute('#/posts/create', function () {
+        // Show the new post page...
+        let data = {
+            fullname: sessionStorage['fullname']
+        };
+        postController.showCreatePostPage(data, authService.isLoggedIn());
+    });
+
+    bindEventHandler('login', function (ev, data) {
+        // Login the user...
+        userController.login(data);
+    });
+
+    bindEventHandler('register', function (ev, data) {
+        // Register a new user...
+        userController.register(data);
+    });
+
+    bindEventHandler('createPost', function (ev, data) {
+        // Create a new post...
+        postController.createPost(data);
+    });
+
+    run('#/');
+})();
